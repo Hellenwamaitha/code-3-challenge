feab04d8637339d0bc5c7b2b69e5b336f2f19d76
hellenwamaitha <hellenwama254@gmail.com>
2023-07-02 20:02:09 +0300

    movie webpage

---
+++ index.js
@@ -0,0 +1,11 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+// Get references to the relevant elements
+const moviePoster = document.querySelector('.movie-poster');
+const movieTitle = document.querySelector('.movie-title');
+const movieRuntime = document.querySelector('.movie-runtime');
+const movieShowtime = document.querySelector('.movie-showtime');
+const availableTicketsCount = document.querySelector('#available-tickets-count');
+const buyTicketBtn = document.querySelector('#buy-ticket-btn');
+const movieMenu = document.querySelector('#films');
+})
